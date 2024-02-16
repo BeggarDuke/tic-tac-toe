0a4cdefceaf2be663fea1457a7e0a95d390bd573
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2024-02-16 16:36:19 +0300

    fix: Fix incorrect status assignment

--- main.js
+++ main.js
@@ -3,52 +3,69 @@ const gameBoard = (function() {
   
   const placeMark = (currentPlayer, choose) => {
     gameBoard[+choose[0]][+choose[1]] = currentPlayer.mark;
-    console.log(gameBoard[0]);
+    console.log(gameBoard);
   }
 
   const showBoard = () => gameBoard;
 
   const gameStatus = () => {
     let status;
-    let winner;
+    let winnerMark;
     for (i=0;i<3;i++) {
       if (gameBoard[i][0] === gameBoard[i][1] 
         && gameBoard[i][0] === gameBoard[i][2] 
         && gameBoard[i][0] !== "") {
         status = "end";
-        winner = `${gameBoard[i][0]}`;  
+        winnerMark = `${gameBoard[i][0]}`;  
       }
       if (gameBoard[0][i] === gameBoard[1][i] 
         && gameBoard[0][i] === gameBoard[2][i] 
         && gameBoard[0][i] !== "") {
         status = "end";
-        winner = `${gameBoard[0][i]}`;  
+        winnerMark = `${gameBoard[0][i]}`;  
       }
     }
     if (gameBoard[0][0] === gameBoard[1][1] 
       && gameBoard[0][0] === gameBoard[2][2] 
       && gameBoard[0][0] !== "") {
-      status = "win";
-      winner = `${gameBoard[0][0]}`;  
+      status = "end";
+      winnerMark = `${gameBoard[0][0]}`;  
     }
     if (gameBoard[0][2] === gameBoard[1][1] 
       && gameBoard[0][2] === gameBoard[2][0] 
       && gameBoard[0][2] !== "") {
       status = "end";
-      winner = `${gameBoard[0][2]}`;  
+      winnerMark = `${gameBoard[0][2]}`;  
     }
       return {
         status,
-        winner,
+        winnerMark,
       }
+  }
+
+  const gameCheck = () => {
+    if (gameStatus().status === "end") {
+      console.log(`Winner is ${gameStatus().winnerMark}`);
+      // gameReset();
+    }
+    else {
+      console.log("123124");
+    }
+    // function gameReset() {
+    //   gameBoard.slice().push(["", "", ""],["", "", ""],["", "", ""]);
+    // }
   }
 
   return {
     placeMark, 
     showBoard,
-    gameStatus,
+    gameCheck,
   }
 })();
+
+const player = (name, mark) => {
+  return {name, mark}
+}
 
 const game = (function() { 
   const player1 = player("a", "x");
@@ -63,21 +80,8 @@ const game = (function() {
     let choose = window.prompt("select row and column: a, b").split(", ");
     if (gameBoard.showBoard()[+choose[0]][+choose[1]] !== "") return;
     gameBoard.placeMark(currentPlayer, choose);
-    gameCheck();
+    gameBoard.gameCheck();
     switchPlayer();
-  }
-
-  const gameCheck = () => {
-    if (gameBoard.gameStatus().status === "end") {
-      console.log(`Winner is ${gameBoard.gameStatus().winner}`);
-      // gameReset();
-    }
-    else {
-      console.log("123124");
-    }
-    function gameReset() {
-      //some func
-    }
   }
 
   return {
@@ -85,6 +89,8 @@ const game = (function() {
   }
 })();
 
-const player = (name, mark) => {
-  return {name, mark}
-}
+
+
+
+// 1) configure winner announcement
+// 2) UI
