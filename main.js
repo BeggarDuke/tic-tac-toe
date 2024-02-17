d332c468f74554ce8bc781cebbd266a0f0c865fc
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2024-02-17 18:41:27 +0300

    feat: finish game logic

--- main.js
+++ main.js
@@ -3,63 +3,14 @@ const gameBoard = (function() {
   
   const placeMark = (currentPlayer, choose) => {
     gameBoard[+choose[0]][+choose[1]] = currentPlayer.mark;
-    console.log(gameBoard);
+    console.table(gameBoard);
   }
 
   const showBoard = () => gameBoard;
 
-  const gameStatus = () => {
-    let status;
-    let winnerMark;
-    for (i=0;i<3;i++) {
-      if (gameBoard[i][0] === gameBoard[i][1] 
-        && gameBoard[i][0] === gameBoard[i][2] 
-        && gameBoard[i][0] !== "") {
-        status = "end";
-        winnerMark = `${gameBoard[i][0]}`;  
-      }
-      if (gameBoard[0][i] === gameBoard[1][i] 
-        && gameBoard[0][i] === gameBoard[2][i] 
-        && gameBoard[0][i] !== "") {
-        status = "end";
-        winnerMark = `${gameBoard[0][i]}`;  
-      }
-    }
-    if (gameBoard[0][0] === gameBoard[1][1] 
-      && gameBoard[0][0] === gameBoard[2][2] 
-      && gameBoard[0][0] !== "") {
-      status = "end";
-      winnerMark = `${gameBoard[0][0]}`;  
-    }
-    if (gameBoard[0][2] === gameBoard[1][1] 
-      && gameBoard[0][2] === gameBoard[2][0] 
-      && gameBoard[0][2] !== "") {
-      status = "end";
-      winnerMark = `${gameBoard[0][2]}`;  
-    }
-      return {
-        status,
-        winnerMark,
-      }
-  }
-
-  const gameCheck = () => {
-    if (gameStatus().status === "end") {
-      console.log(`Winner is ${gameStatus().winnerMark}`);
-      // gameReset();
-    }
-    else {
-      console.log("123124");
-    }
-    // function gameReset() {
-    //   gameBoard.slice().push(["", "", ""],["", "", ""],["", "", ""]);
-    // }
-  }
-
   return {
     placeMark, 
     showBoard,
-    gameCheck,
   }
 })();
 
@@ -68,8 +19,8 @@ const player = (name, mark) => {
 }
 
 const game = (function() { 
-  const player1 = player("a", "x");
-  const player2 = player("b", "o");
+  const player1 = player("Player 1", "x");
+  const player2 = player("Player 2", "o");
   let currentPlayer = player1;
 
   const switchPlayer = () => {
@@ -80,17 +31,76 @@ const game = (function() {
     let choose = window.prompt("select row and column: a, b").split(", ");
     if (gameBoard.showBoard()[+choose[0]][+choose[1]] !== "") return;
     gameBoard.placeMark(currentPlayer, choose);
-    gameBoard.gameCheck();
+    gameCheck();
     switchPlayer();
   }
 
+  const gameStatus = () => {
+    let status = "";
+    let winnerMark = "";
+    for (i=0;i<3;i++) {
+      if (gameBoard.showBoard()[i][0] === gameBoard.showBoard()[i][1] 
+        && gameBoard.showBoard()[i][0] === gameBoard.showBoard()[i][2] 
+        && gameBoard.showBoard()[i][0] !== "") {
+        status = "end";
+        winnerMark = `${gameBoard.showBoard()[i][0]}`;  
+      }
+      if (gameBoard.showBoard()[0][i] === gameBoard.showBoard()[1][i] 
+        && gameBoard.showBoard()[0][i] === gameBoard.showBoard()[2][i] 
+        && gameBoard.showBoard()[0][i] !== "") {
+        status = "end";
+        winnerMark = `${gameBoard.showBoard()[0][i]}`;  
+      }
+    }
+    if (gameBoard.showBoard()[0][0] === gameBoard.showBoard()[1][1] 
+      && gameBoard.showBoard()[0][0] === gameBoard.showBoard()[2][2] 
+      && gameBoard.showBoard()[0][0] !== "") {
+      status = "end";
+      winnerMark = `${gameBoard.showBoard()[0][0]}`;  
+    }
+    if (gameBoard.showBoard()[0][2] === gameBoard.showBoard()[1][1] 
+      && gameBoard.showBoard()[0][2] === gameBoard.showBoard()[2][0] 
+      && gameBoard.showBoard()[0][2] !== "") {
+      status = "end";
+      winnerMark = `${gameBoard.showBoard()[0][2]}`;  
+    }
+      return {
+        status,
+        winnerMark,
+      }
+  }
+
+  const gameCheck = () => {
+    let winner = "";
+    if (gameStatus().status === "end") {
+      winner = (gameStatus().winnerMark === player1.mark) 
+      ? player1.name 
+      : player2.name;
+      console.log(winner);
+      gameReset();
+    }
+    else if (gameBoard.showBoard().every((item) => item === "x" || item === "o")){
+      console.log("tie")
+    }
+    else {
+      console.log("game isn't finished yet");
+    }
+    function gameReset() {
+      gameBoard.showBoard().splice(0);
+      gameBoard.showBoard().push(["", "", ""],["", "", ""],["", "", ""]);
+      gameStatus().status = "";
+      gameStatus().winnerMark = "";
+      winner = "";
+      currentPlayer = player1;
+    }
+  }
+
   return {
+    player1,
+    player2,
     playerTurn,
+    gameCheck,
   }
 })();
 
-
-
-
-// 1) configure winner announcement
-// 2) UI
+// tie condition (every element on array !== "" and status !== "end")
